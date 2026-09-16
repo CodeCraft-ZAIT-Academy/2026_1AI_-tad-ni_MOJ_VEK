cfb4e123d0e9e601f51f90f8fff959e88faec20a
stadania26 <stadania26@Z00T0-02>
2026-09-16 14:13:20 +0200

    Add project files.

---
+++ uloha2/Program.cs
@@ -0,0 +1,6 @@
+﻿DateTime dnes = DateTime.Now;
+Console.WriteLine("zadaj rok");
+string Rok = Console.ReadLine();
+int rok = int.Parse(Rok);
+int vek = dnes.Year - rok;
+Console.WriteLine("Máš"+ vek + "rokov");
